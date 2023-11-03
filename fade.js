ac8638559ab98a336de64260008898f2ad4f82ba
john alain <johnalain572@gmail.com>
2023-11-03 10:17:13 +0200

    fade1

--- fade.js
+++ fade.js
@@ -1,18 +1,16 @@
-$(document).ready(function()
-{
-    //fade in with timer 2s and function perform action after the timer finish
-    // $("button").click(function()
-    // {
-    //     $("div").fadeIn(20000,function(){$("span").show();});
-    //     //fadeIn(speed,callbackfunction)
-    // });
-    //fade out after the timer expired then perform show()action of span
+// https://www.youtube.com/watch?v=ZRupJrMllAQ
+$(document).ready(function () {
+  //fade in with timer 2s and function perform action after the timer finish
+  // $("button").click(function()
+  // {
+  //     $("div").fadeIn(20000,function(){$("span").show();});
+  //     //fadeIn(speed,callbackfunction)
+  // });
+  //fade out after the timer expired then perform show()action of span
 
-    $("button").click(function()
-    {
-        // $("div").fadeOut(2000,function(){$("span").show();});
-        //fadeIn(speed,callbackfunction)
-        $("div").fadeOut(2000,function(){$("span").hide(2000);});
-
-    });
-    });
+  //
+  $("button").click(function () {
+    $("p").hide(3000, alert("paragraph is hidden"));
+    $("button").css("color","#f00")
+  });
+});
